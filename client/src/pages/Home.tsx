4f55d210f1fc2e142de9ff17eda15e071584d565
LiRiK <ggevelurl@gmail.com>
2023-09-29 22:37:51 +0300

    Was added css modules

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import React, { FC } from "react";
 import Menu from "../components/Menu/Menu";
 import Market from "../components/MarketTrends/MarketTrends";
-<<<<<<< refs/remotes/LiRiK/main
-import Team from "../components/Team/module";
-=======
 import Team from "../components/Team/index";
->>>>>>> Make a css module
 import './Home.css'
 
 
