66a6e01d027bf5be617a9baea9155027bff7ebcf
LiRiK <ggevelurl@gmail.com>
2023-09-30 15:11:20 +0300

    Was added Team component

--- client/src/components/Team/index.tsx
+++ client/src/components/Team/index.tsx
@@ -3,73 +3,37 @@ import styles from './index.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Container, Col, Row } from 'react-bootstrap';
 
-const man1 = require('./img/1.png');
+interface TeamProps {
+    cardData: { name: string; position: string; imageIndex: number }[];
+    images: string[];
+    sectionName: string;
+}
 
-
-
-
-
-
-
-const Team: React.FC = () => {
+const Team: React.FC<TeamProps> = (props) => {
     return (
         <Container fluid className={styles.wrapper}>
             <>
-                <div className={styles.SectionName}>
-                    <h1 className={styles.SectionText}>Team</h1>
-                </div>
                 <div className={styles.card_wrapper}>
-            <Row>
-                <Col md={6} lg={3}>
-                    <Card className={styles.card} style={{ background: '#131517' }}>
-                        <Card.Body>
-                            <img src={man1} alt="John Doe" />
-                            <Card.Subtitle className={styles.CardOwnerBlock}>
-                                <span className={styles.CardOwner}>
-                                    John Doe / CEO
-                                </span>
-                            </Card.Subtitle>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={6} lg={3}>
-                    <Card className={styles.card} style={{ background: '#131517' }}>
-                        <Card.Body>
-                            <img src={man1} alt="John Doe" />
-                            <Card.Subtitle className={styles.CardOwnerBlock}>
-                                <span className={styles.CardOwner}>
-                                    John Doe / CEO
-                                </span>
-                            </Card.Subtitle>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={6} lg={3}>
-                    <Card className={styles.card} style={{ background: '#131517' }}>
-                        <Card.Body>
-                            <img src={man1} alt="John Doe" />
-                            <Card.Subtitle className={styles.CardOwnerBlock}>
-                                <span className={styles.CardOwner}>
-                                    John Doe / CEO
-                                </span>
-                            </Card.Subtitle>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={6} lg={3}>
-                    <Card className={styles.card} style={{ background: '#131517' }}>
-                        <Card.Body>
-                            <img src={man1} alt="John Doe" />
-                            <Card.Subtitle className={styles.CardOwnerBlock}>
-                                <span className={styles.CardOwner}>
-                                    John Doe / CEO
-                                </span>
-                            </Card.Subtitle>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            </div>
+                    <Row>
+                        <div className={styles.SectionName}>
+                            <h1 className={styles.SectionText}>{props.sectionName}</h1>
+                        </div>
+                        {props.cardData.map((card, index) => (
+                            <Col md={6} lg={3} key={index}>
+                                <Card className={styles.card} style={{ background: '#131517' }}>
+                                    <Card.Body>
+                                        <img src={props.images[card.imageIndex]} alt={card.name} />
+                                        <Card.Subtitle className={styles.CardOwnerBlock}>
+                                            <span className={styles.CardOwner}>
+                                                {card.name} / {card.position}
+                                            </span>
+                                        </Card.Subtitle>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
             </>
         </Container>
     );
